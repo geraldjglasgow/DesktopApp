329e752ec4c55bce994283ec88ee0cf1ee92ddfe
Gerald Glasgow <geraldjglasgow@gmail.com>
2017-11-02 16:17:20 -0400

    - fixed infinite loop

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ import getdata.Get_Info;
 
 public class Main {
     public static void main(String[] args) {
-        String path = "/Users/SSLGhost/Desktop/ProbemonTests/Test7/";
-        //Process p = new Process(path);
-        //Stats s = new Stats(p.getFilelist());
-        Get_Info data = new Get_Info();
-        data.conn();
+        String path = "/Users/SSLGhost/Desktop/ProbemonTests/Test17/";
+        Process p = new Process(path);
+        Stats s = new Stats(p.getFilelist());
+        //Get_Info data = new Get_Info();
+        //data.conn();
     }
 }
