4df2866eef43948baad70c2435001bde43cc5d7b
Gerald Glasgow <geraldjglasgow@gmail.com>
2017-11-02 15:57:30 -0400

    - added data download and removal

--- src/getdata/Mac.java
+++ src/getdata/Mac.java
@@ -1,34 +1,46 @@
 package getdata;
 
-import java.awt.*;
+import com.jcraft.jsch.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Mac {
-    private String[] files = {
-            "/home/pi/Project/Battery/*.log",
-            "/home/pi/Project/Buttons/*.log",
-            "/home/pi/Project/probe-finder/*.log",
-            "/home/pi/Project/Temperature/*.log"};
 
+public class Mac extends Get_Info{
     private String directory = "/Users/SSLGhost/Desktop";
 
     public Mac(){
         start();
+
     }
 
     private void start(){
-        for(String file : files){
-            //mac_command("scp pi@10.0.0.200:" + file + " " + directory);
-            //mac_command("scp pi@10.0.0.201:" + file + " " + directory);
+        Session session;
+        session = super.getConnPWD();
+        if(session != null){
+            for(String command : super.cmd){
+                send_command(command, session);
+            }
+            mac_command("scp pi@10.0.0.221:" + "/home/pi/Desktop/"+dir+"" + " " + directory);
+            send_command("trash /home/pi/Desktop/"+dir, session);
+            session.disconnect();
         }
-        mac_command("ssh pi@192.168.1.172");
     }
 
     private String mac_command(String cmd) {
 
         try {
             Process p = Runtime.getRuntime().exec(cmd);
+            BufferedReader read = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            BufferedReader err = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            String s;
+            while((s=read.readLine()) != null){
+                System.out.println(s);
+            }
+            while((s=err.readLine()) != null){
+                System.out.println(s);
+            }
+            read.close();
+            err.close();
         } catch(java.io.IOException e){
 
         }
