a122da4bb25c8bab1d6093f3c29940a8bfcddc99
Gerald Glasgow <geraldjglasgow@gmail.com>
2017-11-02 15:53:04 -0400

    - Changed class names in main

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ import getdata.Get_Info;
 
 public class Main {
     public static void main(String[] args) {
-        String path = "/Users/SSLGhost/Desktop/ProbemonTests/Test13/";
+        String path = "/Users/SSLGhost/Desktop/ProbemonTests/Test7/";
         //Process p = new Process(path);
-
         //Stats s = new Stats(p.getFilelist());
         Get_Info data = new Get_Info();
+        data.conn();
     }
 }
