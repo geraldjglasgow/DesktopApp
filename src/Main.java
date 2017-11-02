3b09d327915f85c5b2aa5d6453682f51b563aba2
Gerald Glasgow <geraldjglasgow@gmail.com>
2017-11-02 16:39:32 -0400

    - added system exit

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ import getdata.Get_Info;
 public class Main {
     public static void main(String[] args) {
         String path = "/Users/SSLGhost/Desktop/ProbemonTests/Test17/";
-        Process p = new Process(path);
-        Stats s = new Stats(p.getFilelist());
-        //Get_Info data = new Get_Info();
-        //data.conn();
+        //Process p = new Process(path);
+        //Stats s = new Stats(p.getFilelist());
+        Get_Info data = new Get_Info();
+        data.conn();
+        System.exit(1);
     }
 }
