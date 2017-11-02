bd52db218890b99da266133a7dc2287dc4aaf254
Gerald Glasgow <geraldjglasgow@gmail.com>
2017-11-02 15:59:19 -0400

    - updated time to arraylist

---
+++ src/tree/Node.java
@@ -0,0 +1,36 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+    Node left, right;
+    private String MAC="";
+    private ArrayList<Integer> times = new ArrayList<>();
+
+    public Node(String MAC, int time){
+        this.MAC = MAC;
+        this.times.add(time);
+        left = null;
+        right = null;
+    }
+
+    public Node(){
+        left = null;
+        right = null;
+    }
+
+    public void AddTime(int time){
+        this.times.add(time);
+    }
+
+    public String getMAC(){
+        return this.MAC;
+    }
+    public void printTimes(){
+        for(int i : times){
+            System.out.print(i + " ");
+        }
+    }
+
+}
