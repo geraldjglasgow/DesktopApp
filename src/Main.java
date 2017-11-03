18ba55d3edef22281f436a1077890f8f9a138f6e
Gerald Glasgow <geraldjglasgow@gmail.com>
2017-11-03 01:37:24 -0400

    - fixed objects

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
 import getdata.Get_Info;
+import com.codebind.*;
+import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
-        String path = "/Users/SSLGhost/Desktop/ProbemonTests/Test17/";
-        //Process p = new Process(path);
-        //Stats s = new Stats(p.getFilelist());
-        Get_Info data = new Get_Info();
-        data.conn();
+        String path = "/Users/SSLGhost/Desktop/ProbemonTests/Test19/";
+        Process p = new Process(path);
+        Stats s = new Stats(p.getFilelist());
+        //Get_Info data = new Get_Info();
+        //data.conn();
         System.exit(1);
+//        JFrame frame = new JFrame("APP");
+//        frame.setContentPane(new App().mainPanel);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.pack();
+//        frame.setVisible(true);
     }
 }
