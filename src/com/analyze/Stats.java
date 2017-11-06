3ddbb2caf58aa27135f6613666c4ab0886eb9f55
Gerald Glasgow <geraldjglasgow@gmail.com>
2017-11-06 01:05:18 -0500

    - removed hashsets

--- src/com/analyze/Stats.java
+++ src/com/analyze/Stats.java
@@ -1,61 +1,68 @@
+package com.analyze;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.ArrayList;
+import tree.*;
 
 public class Stats {
 
     private File[] file_list;     // paths to all .log files from devices
-    private List<String> matches; // all matching mac addresses between each file
-    private tree.BTree tree = new tree.BTree();
+    private BTree tree;
+    private HashSet<String> matches = new HashSet<>();
 
     public Stats(File[] list){
         this.file_list = list;
         start();
     }
     private void start(){
+        tree = new BTree();
         read_data();
-        System.out.println(matches.size());
-        //getMatchesTimes();
     }
 
     private void read_data(){
         try {
-            Set<String> unique[] = new HashSet[file_list.length];
             int i=0;
             for (File file : file_list) {
                 String f = file.toString() + "/output.log";
                 BufferedReader Buffone = new BufferedReader(new FileReader(new File(f)));
                 String temp;
-                unique[i] = new HashSet<>();
                 while ((temp = Buffone.readLine()) != null) {
                     try {
-
                         String[] temp1 = temp.split("\t");
-                        tree.insert(temp1[1], Integer.parseInt(temp1[0]));
-                        unique[i].add(temp1[1]);
+                        if(i==0) {
+                            tree.insert(temp1[1], Integer.parseInt(temp1[0]),i);
+                        } else {
+                            Node node;
+                            if((node = tree.find(temp1[1])) != null){
+                                node.AddTime(Integer.parseInt(temp1[0]), i);
+                                matches.add(temp1[1]);
+                            }
+                        }
+
                     } catch (ArrayIndexOutOfBoundsException e) {
                         System.out.println("ERROR: " + e);
                     }
                 }
+                Buffone.close();
                 i+=1;
             }
-            matches = new ArrayList<>(unique[0]);
-            matches.retainAll(unique[1]);
         } catch(java.io.IOException e){
             System.out.println("ERROR: " + e);
         }
     }
 
+    public String getMatches(){
+        return Integer.toString(matches.size());
+    }
+
     public void getMatchesTimes(){
-        for(String temp : matches){
-            tree.Node node = tree.find(temp);
-            if(node != null){
-                System.out.print(node.getMAC() +  " ");
-                //node.printTimes();
+        for (String temp : matches) {
+            Node node = tree.find(temp);
+            if (node != null) {
+                System.out.println(node.getMAC());
+                node.printTimes();
                 System.out.println();
             }
         }
