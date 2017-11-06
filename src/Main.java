43611527060db34a9fd105352643db7da7ae8912
Gerald Glasgow <geraldjglasgow@gmail.com>
2017-11-06 01:10:14 -0500

    - just calls APP

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,12 @@
-import getdata.Get_Info;
-import com.codebind.*;
+import com.app.*;
 import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
-        String path = "/Users/SSLGhost/Desktop/ProbemonTests/Test19/";
-        Process p = new Process(path);
-        Stats s = new Stats(p.getFilelist());
-        //Get_Info data = new Get_Info();
-        //data.conn();
-        System.exit(1);
-//        JFrame frame = new JFrame("APP");
-//        frame.setContentPane(new App().mainPanel);
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.pack();
-//        frame.setVisible(true);
+        JFrame frame = new JFrame("APP");
+        frame.setContentPane(new App().panelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
